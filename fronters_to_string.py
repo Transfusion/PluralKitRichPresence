894b5b200c18b72be8c66187d6ef28b5d6f8ea22
Bryan Kok <transfusian@gmail.com>
2020-11-29 03:00:06 +0800

    Sample module; customizing the details and status

---
+++ fronters_to_string.py
@@ -0,0 +1,24 @@
+"""
+A sample module that turns a dict with type
+https://app.swaggerhub.com/apis-docs/xSke/PluralKit/1.1#/Systems/GetSystemFronters
+into {"details": string, "state": string}.
+Must contain a fronters_to_string function.
+"""
+
+
+def fronters_to_string(system_info, fronters):
+    members = fronters["members"]
+
+    details = ", ".join(
+        map(
+            lambda member: member["display_name"]
+            if member["display_name"] is not None
+            else member["name"],
+            members,
+        )
+        if len(members)
+        else "(none)"
+    )
+
+    state = system_info["name"] if system_info["name"] is not None else "---"
+    return {"details": details, "state": state}
